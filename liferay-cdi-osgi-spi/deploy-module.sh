50d3af4f55c0c144bd0c371c0255ac5e108032fd
Neil Griffin <neil.griffin.scm@gmail.com>
2016-11-07 13:07:54 -0500

    Added deploy-module.sh convenience scripts

---
+++ liferay-cdi-osgi-spi/deploy-module.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+JSF_VERSION=2.2
+LIFERAY_VERSION=7.0.3-SNAPSHOT
+LIFERAY_HOME=$PORTALS_HOME/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION
+mvn clean install
+cp target/com.liferay.cdi.osgi.spi*.jar $LIFERAY_HOME/osgi/configs/com.liferay.cdi.osgi.spi.jar
