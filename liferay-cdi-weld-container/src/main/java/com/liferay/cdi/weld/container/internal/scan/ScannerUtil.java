066c030594274eb7a25c39d56db44b811c997016
Raymond Auge <raymond.auge@liferay.com>
2016-11-21 21:17:39 -0500

    license

--- liferay-cdi-weld-container/src/main/java/com/liferay/cdi/weld/container/internal/scan/ScannerUtil.java
+++ liferay-cdi-weld-container/src/main/java/com/liferay/cdi/weld/container/internal/scan/ScannerUtil.java
@@ -1,16 +1,19 @@
 /**
  * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package com.liferay.cdi.weld.container.internal.scan;
 
 import java.net.URL;
@@ -19,14 +22,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.osgi.framework.Bundle;
 import org.osgi.framework.wiring.BundleRequirement;
 import org.osgi.framework.wiring.BundleWiring;
 import org.osgi.service.cdi.Constants;
 
-/**
- * @author  Neil Griffin
- */
 public class ScannerUtil {
 
 	private static final String[] BEAN_DESCRIPTOR_PATHS = new String[] {"META-INF/beans.xml", "WEB-INF/beans.xml"};
@@ -45,7 +44,7 @@ public class ScannerUtil {
 		List<String> beanClasses = new ArrayList<String>();
 
 		List<BundleRequirement> requirements = bundleWiring.getRequirements(Constants.CDI_EXTENDER);
-		
+
 		for (BundleRequirement requirement : requirements) {
 			Map<String, Object> attributes = requirement.getAttributes();
 			if (attributes.containsKey("beans")) {
@@ -59,12 +58,12 @@ public class ScannerUtil {
 		if (beanClasses.isEmpty()) {
 			Collection<String> resources = bundleWiring.listResources(
 				"/", "*.class", BundleWiring.LISTRESOURCES_LOCAL | BundleWiring.LISTRESOURCES_RECURSE);
-	
+
 			if (resources != null) {
 				for (String resource : resources) {
 					resource = resource.replace('/', '.');
 					resource = resource.replace(".class", "");
-		
+
 					beanClasses.add(resource);
 				}
 			}
