8b9e9ffdd39c081d2998f015fa785ffbd4a887b2
Raymond Auge <raymond.auge@liferay.com>
2016-11-25 09:38:28 -0500

    remove external POC code

--- liferay-cdi-weld-jsf/deploy-module.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-JSF_VERSION=2.2
-LIFERAY_VERSION=7.0.3-SNAPSHOT
-LIFERAY_HOME=$PORTALS_HOME/liferay.com/liferay-portal-$LIFERAY_VERSION-jsf-$JSF_VERSION
-mvn clean install
-cp target/com.liferay.cdi.weld.jsf*.jar $LIFERAY_HOME/osgi/configs/com.liferay.cdi.weld.jsf.jar
